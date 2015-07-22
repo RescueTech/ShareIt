3ec55fba206ca05904a7792f471266a24a3755ec
Ahmed khan <ahmeds.elghareeb@gmail.com>
2015-07-22 22:02:34 +0200

    Add UI to the DVR page.

--- video_share/urls.py
+++ video_share/urls.py
@@ -5,6 +5,6 @@ from .views import VideoView
 
 urlpatterns = [
     url(r'^$', login_required(VideoView.as_view()), name='live_stream'),
-    url(r'^dvr/$', TemplateView.as_view(template_name="dvr.html"), name='live_stream'),
+    url(r'^dvr/$', login_required(TemplateView.as_view(template_name="dvr.html")), name='dvr_stream'),
 
 ]
